888ab7752d4536ed1f0035f4d2414b833f4b1e08
Keosh <106485828+keosh1@users.noreply.github.com>
2024-09-28 20:30:17 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/keosh1/local_manifests.git -b flake .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
+/opt/crave/resync.sh
+export BUILD_USERNAME=keosh1
+export BUILD_HOSTNAME=crave
+echo "============="
+echo "Sync success"
+echo "============="
+# Apply all the patches.
+# You need to reapply the patch after every sync
+sudo apt install python3
+git clone https://github.com/flakeforever/device_xiaomi_mondrian-patch.git
+python device/xiaomi/mondrian-patch/apply-patches.py
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+lunch mondrian-user
+make installclean
+mka bacon
