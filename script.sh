d755abe6d54768483eeb55f71caf2794fda55449
Keosh <106485828+keosh1@users.noreply.github.com>
2024-10-01 14:04:42 +0300

    (1)

--- script.sh
+++ script.sh
@@ -21,15 +21,12 @@ echo "============="
 echo "Sync success"
 echo "============="
 # Apply all the patches.
-# You need to reapply the patch after every sync
 #cherry-pick
 git fetch https://github.com/DanipunK1-mondrian-DC/external_wpa_supplicant_8.git
 git cherry-pick e8faf37f10bc06610d682c8fa3fc6c8c1c93fc79
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
-lunch lineage_mondrian-userdebug
+riseup mondrian userdebug
 make installclean
-mka bacon
-#croot
 rise b
