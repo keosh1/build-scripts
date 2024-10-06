615a1692b36c5cc5dd3286735ce788d4fdddafaa
Keosh <106485828+keosh1@users.noreply.github.com>
2024-10-06 21:34:46 +0300

    try this new script :)

---
+++ RisingOS14.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b los21 https://github.com/keosh1/local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=keosh1
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# ...and run our repopicks script
+bash https://raw.githubusercontent.com/keosh1/local_manifests/refs/heads/los21/repopicks.sh
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+riseup mondrian userdebug
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+rise b
